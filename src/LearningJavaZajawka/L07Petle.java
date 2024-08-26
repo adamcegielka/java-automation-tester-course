c3f8b2019bdf093ae2992f54b9121c209004b645
Adam Cegiełka <adamcegielka@tlen.pl>
2024-08-26 19:30:22 +0200

    test: add lesson with loops

--- src/LearningJavaZajawka/L07Petle.java
+++ src/LearningJavaZajawka/L07Petle.java
@@ -3,9 +3,11 @@ package LearningJavaZajawka;
 import java.util.ArrayList;
 import java.util.List;
 
-public class L07PetleWhile {
+public class L07Petle {
     public static void main(String[] args) {
         exampleWhile();
+        exampleWhileBreak();
+        exampleWhileContinue();
         exampleDoWhile();
         exampleFor();
         exampleForArray();
@@ -14,41 +16,101 @@ public class L07PetleWhile {
 
     // Example of a While loop
     public static void exampleWhile() {
-        int a = 7;
+        int a = 4;
         while (a > 0) {
             System.out.println("a = " + a);
             a--;
         }
         System.out.println("While loop completed\n");
+        /*
+        a = 4
+        a = 3
+        a = 2
+        a = 1
+        While loop completed
+        */
+    }
+
+    // Example of a While loop with Break
+    public static void exampleWhileBreak() {
+        int b = 4;
+        while (true) {
+            b--;
+            System.out.println("b = " + b);
+            if (b == 2) {
+                break;
+            }
+        }
+        System.out.println("While loop with Break completed\n");
+        /*
+        b = 3
+        b = 2
+        While loop with Break completed
+        */
+    }
+
+    // Example of a While loop with Break
+    public static void exampleWhileContinue() {
+        int c = 4;
+        while (c > 0) {
+            c--;
+            if (c == 2) {
+                continue;
+            }
+            System.out.println("c = " + c);
+        }
+        System.out.println("While loop with Continue completed\n");
+        /*
+        c = 3
+        c = 1
+        c = 0
+        While loop with Continue completed
+        */
     }
 
     // Example of a Do-While loop
     public static void exampleDoWhile() {
-        int b = 10;
+        int d = 6;
         do {
-            System.out.println("c = " + b);
-            b--;
-        } while (b > 0);
+            System.out.println("d = " + d);
+            d--;
+        } while (d > 9);
 
         System.out.println("Do-while loop completed\n");
+        /*
+        d = 6
+        Do-while loop completed
+        */
     }
 
     // Example of a for loop
     public static void exampleFor() {
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 0; i < 3; i++) {
             System.out.println("i = " + i);
         }
         System.out.println("For loop completed\n");
+        /*
+        i = 0
+        i = 1
+        i = 2
+        For loop completed
+        */
     }
 
     // Example of a for loop iterating through an array
     public static void exampleForArray() {
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {1, 2, 3};
 
         for(int i = 0; i < numbers.length; i++) {
             System.out.println("Array element = " + numbers[i]);
         }
         System.out.println("Iteration over the array completed\n");
+        /*
+        Array element = 1
+        Array element = 2
+        Array element = 3
+        Iteration over the array completed
+        */
     }
 
     // Example of a for-each loop iterating through the elements of a list
@@ -62,6 +124,12 @@ public class L07PetleWhile {
             System.out.println("Name = " + name);
         }
         System.out.println("For-each iteration completed\n");
+        /*
+        Name = Jan
+        Name = Anna
+        Name = Piotr
+        For-each iteration completed
+        */
     }
 }
 
